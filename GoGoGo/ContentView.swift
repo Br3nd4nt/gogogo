da0c4520ad1aedc63afcd998ba8dce7c5dd6578e
Br3nd4nt <Vlad.belov.08@gmail.com>
2026-06-28 00:04:49 +0300

    Initial Commit

---
+++ GoGoGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoGoGo
+//
+//  Created by br3nd4nt on 28.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoGoGo/GoGoGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoGoGoApp.swift
+//  GoGoGo
+//
+//  Created by br3nd4nt on 28.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct GoGoGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
